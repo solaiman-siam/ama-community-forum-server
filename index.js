38393205a8528a1c75d0c436586520f1034e9c2e
solaiman-siam <solaimansiam93@gmail.com>
2024-06-03 00:16:04 +0600

    added user route

--- index.js
+++ index.js
@@ -1,15 +1,75 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
+// middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+  })
+);
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8vxmi4o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("amaDB").collection("users");
+
+    // save new logged  user info
+    app.post("/users", async (req, res) => {
+      const userData = req.body;
+
+      const query = { email: userData?.email };
+
+      const isExist = await userCollection.findOne(query);
+      if (isExist) {
+        return;
+      }
+
+      const result = await userCollection.insertOne(userData);
+      res.send(result);
+    });
+
+    // get logged user
+    app.get("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// testing method
 app.get("/", (req, res) => {
   res.send("ama is running");
 });
 
 app.listen(port, () => {
-  console.log("ama is running por", port);
+  console.log("ama is running port", port);
 });
